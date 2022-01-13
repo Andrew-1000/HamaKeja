bfc9b5b8f664c89a6df382639070c4100f18216e
Andrew-1000 <andruambia95@gmail.com>
2022-01-13 17:45:14 +0300

    fragment instantiated in MainActivity file

--- app/src/main/java/com/example/hamakeja/MainActivity.kt
+++ app/src/main/java/com/example/hamakeja/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.hamakeja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+    private val message = "Welcome Back"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        class OwnerFragment : Fragment(R.layout.fragment_owner);
+    }
+
+    fun welcomeBack(view: android.view.View) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
